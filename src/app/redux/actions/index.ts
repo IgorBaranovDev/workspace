27cf7a0c71960b68167e837d556f7230d5f9c8d5
IgorBaranovDev <baranovigorjsdev@gmail.com>
2021-07-21 11:41:49 +0300

    feat-JSMP-4: created actions auth

--- src/app/redux/actions/index.ts
+++ src/app/redux/actions/index.ts
@@ -1,2 +1,14 @@
 // создать экшены для начала авторизации, успешная, провал авторизации. type string payload: {} || ''
-// функции авторизация старт, данные подьзователя,  
+// функции авторизация старт, данные подьзователя
+
+const LOGIN_REQUEST = { type: "USERS_LOGIN_REQUEST", payload: {} };
+const LOGIN_SUCCESS = { type: "USERS_LOGIN_SUCCESS", payload: {} };
+const LOGIN_FAILURE = { type: "USERS_LOGIN_FAILURE", payload: {} };
+const LOGOUT = { type: "USERS_LOGOUT", payload: {} };
+
+export  {
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  LOGOUT,
+};
