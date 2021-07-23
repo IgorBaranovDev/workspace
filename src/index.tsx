db67f6362bdcb6cba50eb8c709c34e525fa0b088
IgorBaranovDev <baranovigorjsdev@gmail.com>
2021-07-23 16:45:48 +0300

    feat-JSMP-4: add Provider for store in App

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,20 @@ import ReactDOM from "react-dom";
 import App from "./app";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./app/redux/store";
+
+// material-ui
+import { CssBaseline } from "@material-ui/core";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <CssBaseline />
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
