020e038a6ea600f2ec3fb768fda428fafa7a5152
IgorBaranovDev <baranovigorjsdev@gmail.com>
2021-07-23 10:00:56 +0300

    feat-JSMP-4: created authReducer

--- src/app/redux/reducers/authReducer.ts
+++ src/app/redux/reducers/authReducer.ts
@@ -1,1 +1,29 @@
-// редусер получает state action, обновить стэйт после авторизации. дефолт експорт 
+// редусер получает state, action
+// => обновить стэйт после авторизации. дефолт експорт
+
+// actions
+import {
+  // LOGIN_REQUEST,
+  // SINGUP_REQUEST,
+  AUTH_SUCCESS,
+  // AUTH_FAILURE,
+  LOGOUT,
+} from "../actions";
+
+// types
+import { Action } from "../actions/types";
+
+const initialState = {
+  user: null, // auth.user: jjj@dd
+};
+
+export default function authReducer(state = initialState, { type, payload }: Action) {
+  switch (type) {
+    case AUTH_SUCCESS:
+      return { ...state, user: payload };
+    case LOGOUT:
+      return { ...state, user: null };
+    default:
+      return state;
+  }
+};
