f7cea91a8c20eb4de50afaaf71fa33cb7efb8ddf
IgorBaranovDev <baranovigorjsdev@gmail.com>
2021-07-29 14:23:42 +0300

    feat-JSMP-5: fixed selectors

--- src/app/redux/selectors/index.ts
+++ src/app/redux/selectors/index.ts
@@ -1,2 +1,7 @@
-// selector
+// types
+import { OfficesData } from "../../services/BD/type/OfficesData";
+
+// selectors
 export const getAuthUser = (state: { auth: { user: string } }) => state.auth.user;
+
+export const getOfficesData = (state: {officesData: OfficesData}) => state.officesData;
