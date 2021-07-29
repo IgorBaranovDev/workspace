5963ef418cb15652ef5d31883ca6ef14641f2f74
IgorBaranovDev <baranovigorjsdev@gmail.com>
2021-07-29 13:50:40 +0300

    feat-JSMP-5: created Actions for data offices

--- src/app/redux/actions/selectOffice.ts
+++ src/app/redux/actions/selectOffice.ts
@@ -1,14 +1,28 @@
+import { Floors } from "../../services/BD/type/Floors";
+import { OfficesData } from "../../services/BD/type/OfficesData";
 import { Action } from "./types";
-import { OfficesDate } from '../../services/BD/type';
 
-export const FETCH_DATA = 'FETCH_DATA';
-export const WRITE_DATA = 'WRITE_DATA'; 
+export const FETCH_OFFICES_DATA = 'FETCH_OFFICES_DATA';
+export const FETCH_OFFICES_DATA_COMPLETE = 'FETCH_OFFICES_DATA_COMPLETE';
+export const FETCH_FLOORS_DATA = 'FETCH_FLOORS_DATA';
+export const FETCH_FLOORS_DATA_COMPLETE = 'FETCH_FLOORS_DATA_COMPLETE';
 
-export const fechData = (): Action => ({
-    type: FETCH_DATA
+export const fetchOfficesData = (): Action => ({
+    type: FETCH_OFFICES_DATA,
+});
+
+export const fetchOfficesDataComplelte = (officesData: OfficesData): Action => ({
+    type: FETCH_OFFICES_DATA_COMPLETE,
+    payload: officesData
+});
+
+export const fetchFloorsData = (id: string): Action => ({
+    type: FETCH_FLOORS_DATA,
+    payload: id
+});
+
+export const fetchFloorsDataComplelte = (floors: Floors): Action => ({
+    type: FETCH_FLOORS_DATA_COMPLETE,
+    payload: floors
 })
 
-export const writeData = (data: OfficesDate): Action => ({
-    type: WRITE_DATA,
-    payload: data
-})
