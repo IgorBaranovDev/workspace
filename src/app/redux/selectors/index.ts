4e12e93da03153a00bac6fbb56a5346e92860942
IgorBaranovDev <baranovigorjsdev@gmail.com>
2021-08-11 14:10:54 +0300

    feat-JSMP-7: fixed selector - getFloorsData

--- src/app/redux/selectors/index.ts
+++ src/app/redux/selectors/index.ts
@@ -1,10 +1,13 @@
 // types
 import { OfficesData } from "../../services/BD/type/OfficesData";
-import { Floors } from '../../services/BD/type/Floors';
+import { Floors } from "../../services/BD/type/Floors";
 
 // selectors
-export const getAuthUser = (state: { auth: { user: string } }) => state.auth.user;
+export const getAuthUser = (state: { auth: { user: string } }) =>
+  state.auth.user;
 
-export const getOfficesData = (state: {officesData: OfficesData}) => state.officesData;
+export const getOfficesData = (state: { officesData: OfficesData }) =>
+  state.officesData;
 
-export const getFloorsData = (state: {floors: Floors}) => state.floors;
+export const getFloorsData = (state: { workspaces: { floors: Floors } }) =>
+  state.workspaces.floors;
