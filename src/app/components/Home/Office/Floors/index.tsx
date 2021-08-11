4f8194542a966e6d5139fe8176f34271fc08ea71
IgorBaranovDev <baranovigorjsdev@gmail.com>
2021-08-11 18:00:05 +0300

    feat-JSMP-7: created component Floors, when user can select floor of office

--- src/app/components/Home/Office/Floors/index.tsx
+++ src/app/components/Home/Office/Floors/index.tsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+
+// style
+import { InputItem, Select } from "../../GroupSelects/components";
 
 interface IFloors {
   dataFloors: Array<{ [key: string]: number }>;
 }
 
 const Floors: React.FC<IFloors> = ({ dataFloors }) => {
-  console.log(dataFloors)
-  return <>
-  <p>Floors</p>
-  </>;
+  console.log(dataFloors);
+  const [floor, setFloor] = useState("");
+
+  const handleChange = (event: React.FormEvent<HTMLSelectElement>) => {
+    const element = event.target as HTMLSelectElement;
+    setFloor(element.value as string);
+  };
+
+  return (
+    <>
+      <p>Floors: {dataFloors.length}</p>
+      <InputItem>
+        <Select
+          id="selectFloor"
+          name="selectFloor"
+          value={floor}
+          onChange={handleChange}
+        >
+          <option hidden value="">
+            Select Floor
+          </option>
+          {dataFloors?.map((item, index) => (
+            <option value={`${item}`} key={index}>
+              floor - {item}
+            </option>
+          ))}
+        </Select>
+      </InputItem>
+    </>
+  );
 };
 
 export default Floors;
