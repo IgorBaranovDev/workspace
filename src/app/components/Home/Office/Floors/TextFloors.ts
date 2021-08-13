e2badaf81f63983089b61f6f2053004f30b7771a
IgorBaranovDev <baranovigorjsdev@gmail.com>
2021-08-13 12:45:37 +0300

    style-JSMP-7: added some styles for component - Office

---
+++ src/app/components/Home/Office/Floors/TextFloors.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const TextFloors = styled.p`
+    font-size: 16px;
+    text-transform: capitalize;
+`;
+
+export default TextFloors;
---
+++ src/app/components/Home/Office/components/TitleOffice.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const TitleOffice = styled.h2`
+  margin-left: 15px;
+`;
+
+export default TitleOffice;
