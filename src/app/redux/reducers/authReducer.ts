53dd43ecb1084ee2f5bee4f710103323a80c1e8b
IgorBaranovDev <baranovigorjsdev@gmail.com>
2021-08-18 10:32:26 +0300

    feat-JSMP-8: created action - LOADING and updated reducer

--- src/app/redux/reducers/authReducer.ts
+++ src/app/redux/reducers/authReducer.ts
@@ -1,11 +1,19 @@
 // actions
-import { AUTH_SUCCESS, LOGOUT } from "../actions";
+import {
+  AUTH_SUCCESS,
+  LOGOUT,
+  LOGIN_REQUEST,
+  SINGUP_REQUEST,
+  GET_CURRENT_USER,
+  AUTH_FAILURE,
+} from "../actions";
 
 // types
 import { Action } from "../actions/types";
 
 const initialState = {
   user: null,
+  loading: false,
 };
 
 export default function authReducer(
@@ -15,9 +23,17 @@ export default function authReducer(
   // console.log('RUN REDUCER');
   switch (type) {
     case AUTH_SUCCESS:
-      return { ...state, user: payload };
+      return { ...state, user: payload, loading: false };
     case LOGOUT:
       return { ...state, user: null };
+    case LOGIN_REQUEST:
+      return { ...state, loading: true };
+    case SINGUP_REQUEST:
+      return { ...state, loading: true };
+    case GET_CURRENT_USER:
+      return { ...state, loading: true };
+    case AUTH_FAILURE:
+      return { ...state, loading: false };
     default:
       return state;
   }
