7a6b2df34557a02dedeacd64b99971f131800445
IgorBaranovDev <baranovigorjsdev@gmail.com>
2021-08-20 16:20:53 +0300

    feat-JSMP-10: implemented correct types for BD

---
+++ src/app/services/BD/type/index.ts
@@ -0,0 +1,7 @@
+import { OfficesData } from "./OfficesData";
+import { Offices } from "./Offices";
+import { SelectedOffice } from "./SelectedOffice";
+import { Addres } from "./Addres";
+import { Floors } from "./Floors";
+
+export type { OfficesData, Offices, SelectedOffice, Addres, Floors };
