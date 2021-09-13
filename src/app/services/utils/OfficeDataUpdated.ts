8391a564983e445af6c926f57879eae95294b663
IgorBaranovDev <baranovigorjsdev@gmail.com>
2021-09-13 18:32:49 +0300

    refactor-JSMP-16: modified function - OfficeDataUpdated

--- src/app/services/utils/OfficeDataUpdated.ts
+++ src/app/services/utils/OfficeDataUpdated.ts
@@ -7,25 +7,26 @@ import { SelectedOffice } from "../BD/type";
 import { FormatDate } from "./FormatDate";
 
 export const OfficeDataUpdated = (selectedOffice: SelectedOffice) => {
+  const dateNow = new Date();
   const result = {
     ...selectedOffice,
     floors: selectedOffice.floors.map((floor) => ({
       ...floor,
-      places: floor.places.map((place) => ({
-        ...place,
-        placeStatus: {
-          ...place.placeStatus,
-          end: isAfter(new Date(), new Date(place.placeStatus.end))
-            ? FormatDate(new Date())
-            : place.placeStatus.end,
-          start: isAfter(new Date(), new Date(place.placeStatus.end))
-            ? FormatDate(new Date())
-            : place.placeStatus.start,
-          occupant: isAfter(new Date(), new Date(place.placeStatus.end))
-            ? ""
-            : place.placeStatus.occupant,
-        },
-      })),
+      places: floor.places.map((place) => {
+        const endReservation = new Date(place.placeStatus.end);
+        const resultCompare = isAfter(dateNow, endReservation);
+        return {
+          ...place,
+          placeStatus: {
+            ...place.placeStatus,
+            end: resultCompare ? FormatDate(dateNow) : place.placeStatus.end,
+            start: resultCompare
+              ? FormatDate(dateNow)
+              : place.placeStatus.start,
+            occupant: resultCompare ? "" : place.placeStatus.occupant,
+          },
+        };
+      }),
     })),
   };
 
