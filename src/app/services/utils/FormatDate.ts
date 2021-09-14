5fd3c140f908bb7376578a8b5765a3a32b180e98
IgorBaranovDev <baranovigorjsdev@gmail.com>
2021-09-14 10:16:20 +0300

    fix-JSMP-16: fixed after review #2

--- src/app/services/utils/FormatDate.ts
+++
@@ -1,8 +0,0 @@
-export const FormatDate = (date: Date): string => {
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
-    const monthFormat = (x: any) => (x < 10 ? "0" + x : x);
-    const dateNowFormat = `${year}-${monthFormat(month)}-${day}`;
-    return dateNowFormat;
-  };  
